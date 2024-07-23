09f3d966c40b6cfab4af35441854eea0269564c9
Wamitinewton <wamitinewton@gmail.com>
2024-07-23 13:25:49 +0300

    audio classification listener

--- app/src/main/java/com/example/audio_kotlin/fragments/AudioFragment.kt
+++ app/src/main/java/com/example/audio_kotlin/fragments/AudioFragment.kt
@@ -1,8 +1,14 @@
 package com.example.audio_kotlin.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.audio_kotlin.AudioClassificationHelper
 import com.example.audio_kotlin.databinding.FragmentAudioBinding
+import com.example.audio_kotlin.ui.ProbabilitiesAdapter
 import org.tensorflow.lite.support.label.Category
 
 interface AudioClassificationListener{
@@ -14,15 +20,45 @@ class AudioFragment : Fragment() {
     private var _fragmentBinding: FragmentAudioBinding? = null
     private val fragmentAudioBinding get() = _fragmentBinding!!
     private val adapter by lazy { // calling the probability adapter here
+        ProbabilitiesAdapter()
     }
 
     private lateinit var audioHelper: AudioClassificationHelper
 
-    private val audioClassificationListener = object: AudioClassificationHelper {
+    private val audioClassificationListener = object : AudioClassificationListener {
         override fun onResult(results: List<Category>, inferenceTime: Long) {
             requireActivity().runOnUiThread {
                 adapter.categoryList = results
+                adapter.notifyDataSetChanged()
+                fragmentAudioBinding.bottomSheetLayout.inferenceTimeVal.text =
+                    String.format("%d ms", inferenceTime)
             }
         }
+
+        override fun onError(error: String) {
+            requireActivity().runOnUiThread {
+                Toast.makeText(requireContext(), error, Toast.LENGTH_SHORT).show()
+                adapter.categoryList = emptyList()
+                adapter.notifyDataSetChanged()
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _fragmentBinding = FragmentAudioBinding.inflate(inflater, container, false)
+        return fragmentAudioBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fragmentAudioBinding.recyclerView.adapter = adapter
+        audioHelper = AudioClassificationHelper(
+            requireContext(),
+            audioClassificationListener
+        )
     }
 }
