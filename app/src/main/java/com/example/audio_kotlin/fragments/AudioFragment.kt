29bfffa80d5b284042f2272545c54f59b041929f
Wamitinewton <wamitinewton@gmail.com>
2024-07-23 16:01:38 +0300

    cpu fragment Audio Binding options

--- app/src/main/java/com/example/audio_kotlin/fragments/AudioFragment.kt
+++ app/src/main/java/com/example/audio_kotlin/fragments/AudioFragment.kt
@@ -3,10 +3,13 @@ package com.example.audio_kotlin.fragments
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.AdapterView
 import android.view.ViewGroup
+import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.audio_kotlin.AudioClassificationHelper
+import com.example.audio_kotlin.R
 import com.example.audio_kotlin.databinding.FragmentAudioBinding
 import com.example.audio_kotlin.ui.ProbabilitiesAdapter
 import org.tensorflow.lite.support.label.Category
@@ -43,7 +46,7 @@ class AudioFragment : Fragment() {
             }
         }
     }
-
+// the following are liecycle methods
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -60,5 +63,116 @@ class AudioFragment : Fragment() {
             requireContext(),
             audioClassificationListener
         )
+
+        fragmentAudioBinding.bottomSheetLayout.modelSelector.setOnCheckedChangeListener(
+            object : RadioGroup.OnCheckedChangeListener{
+                override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+                    when (checkedId) {
+                        R.id.yamnet -> {
+                            audioHelper.stopAudioClassification()
+                            audioHelper.currentModel = AudioClassificationHelper.YAMNET_MODEL
+                            audioHelper.initClassify()
+                        }
+                        R.id.speech_command -> {
+                            audioHelper.stopAudioClassification()
+                            audioHelper.currentModel = AudioClassificationHelper.SPEECH_COMMAND_MODEL
+                            audioHelper.initClassify()
+                        }
+                    }
+                }
+            }
+        )
+
+        fragmentAudioBinding.bottomSheetLayout.spinnerOverlap.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    audioHelper.stopAudioClassification()
+                    audioHelper.overlap = 0.25f * position
+                    audioHelper.startAudioClassification()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    // nothing to call here
+                }
+            }
+        // Allow the user to change the maximum number of results returned by the audio classifier
+        // currently allow between 1 and 5 results, but can be edited here
+
+        fragmentAudioBinding.bottomSheetLayout.resultsMinus.setOnClickListener {
+            if (audioHelper.numOfResults > 1) {
+                audioHelper.numOfResults--
+                audioHelper.stopAudioClassification()
+                audioHelper.initClassify()
+                fragmentAudioBinding.bottomSheetLayout.resultsValue.text =
+                    audioHelper.numOfResults.toString()
+            }
+        }
+
+        fragmentAudioBinding.bottomSheetLayout.resultsPlus.setOnClickListener {
+            if (audioHelper.numOfResults < 5){
+                audioHelper.numOfResults++
+                audioHelper.initClassify()
+                fragmentAudioBinding.bottomSheetLayout.resultsValue.text =
+                    audioHelper.numOfResults.toString()
+            }
+        }
+
+        // Allow the user to change the confidence threshold for the audio classifier to return
+        // a result. Increments in steps of 0.1 or 10%.
+
+        fragmentAudioBinding.bottomSheetLayout.thresholdMinus.setOnClickListener {
+            if (audioHelper.classificationThreshhold >= 0.2){
+                audioHelper.stopAudioClassification()
+                audioHelper.classificationThreshhold -= 0.1f
+                audioHelper.initClassify()
+                fragmentAudioBinding.bottomSheetLayout.thresholdValue.text =
+                    String.format("%.2f", audioHelper.classificationThreshhold)
+            }
+        }
+
+        fragmentAudioBinding.bottomSheetLayout.thresholdPlus.setOnClickListener {
+            if (audioHelper.classificationThreshhold <= 0.8) {
+                audioHelper.stopAudioClassification()
+                audioHelper.classificationThreshhold += 0.1f
+                audioHelper.initClassify()
+                fragmentAudioBinding.bottomSheetLayout.threadsValue.text =
+                    String.format("%.2f", audioHelper.classificationThreshhold)
+            }
+        }
+
+        // allow the user to change the number of threads used for classification
+        fragmentAudioBinding.bottomSheetLayout.threadsMinus.setOnClickListener {
+            if (audioHelper.numThreads > 1){
+                audioHelper.stopAudioClassification()
+                audioHelper.numThreads--
+                fragmentAudioBinding.bottomSheetLayout.threadsValue.text = audioHelper
+                    .numThreads
+                    .toString()
+                audioHelper.initClassify()
+            }
+        }
+
+        fragmentAudioBinding.bottomSheetLayout.threadsPlus.setOnClickListener {
+            if (audioHelper.numThreads < 4){
+                audioHelper.stopAudioClassification()
+                audioHelper.numThreads++
+                fragmentAudioBinding.bottomSheetLayout.threadsValue.text = audioHelper
+                    .numThreads
+                    .toString()
+                audioHelper.initClassify()
+            }
+        }
+
+        // When clicked, change the underlying hardware used for inference. Current options are CPU
+        // and NNAPI. GPU is another available option, but when using this option you will need
+        // to initialize the classifier on the thread that does the classifying. This requires a
+        // different app structure than is used in this sample.
+
+
     }
 }
