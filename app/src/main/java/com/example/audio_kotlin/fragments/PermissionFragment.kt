4bc7613338fb37d011a1ae45d356a2058fe7430c
Wamitinewton <wamitinewton@gmail.com>
2024-07-23 17:40:49 +0300

    fixed bug on navgraphs.xml and modified gradle plugins

--- app/src/main/java/com/example/audio_kotlin/fragments/PermissionFragment.kt
+++ app/src/main/java/com/example/audio_kotlin/fragments/PermissionFragment.kt
@@ -1,12 +1,16 @@
 package com.example.audio_kotlin.fragments
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.Navigation
+import com.example.audio_kotlin.R
 
 private val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.RECORD_AUDIO)
 
@@ -32,7 +36,27 @@ class PermissionFragment: Fragment() {
                 Manifest.permission.RECORD_AUDIO
             ) == PackageManager.PERMISSION_GRANTED -> {
                 // navigate here
+                navigateToAudioFragment()
             }
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.RECORD_AUDIO
+                )
+            }
+        }
+    }
+
+    private fun navigateToAudioFragment() {
+        lifecycleScope.launchWhenStarted {
+            Navigation.findNavController(requireActivity(), R.id.fragment_container).navigate(
+                PermissionFragmentDirections.actionPermissionsToAudio()
+            )
+        }
+    }
+    companion object {
+        /** Convenience method used to check if all permissions required by this app are granted */
+        fun hasPermissions(context: Context) = PERMISSIONS_REQUIRED.all {
+            ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
         }
     }
 }
